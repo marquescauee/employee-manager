6e0af4d78fc1008bef3507023d99d1e5acd0704a
marquescauee <cauelopesmarques123@gmail.com>
2023-02-02 21:43:48 -0300

    Redefining routes

--- client/src/services/EmployeeService.js
+++ client/src/services/EmployeeService.js
@@ -5,23 +5,23 @@ const EMPLOYEE_API_BASE_URL = "http://localhost:8080/api"
 class EmployeeService {
 
     saveEmployee = (employee) => {
-        return axios.post(`${EMPLOYEE_API_BASE_URL}/addEmployee`, employee);
+        return axios.post(`${EMPLOYEE_API_BASE_URL}`, employee);
     }
 
     getEmployees = () => {
-        return axios.get(`${EMPLOYEE_API_BASE_URL}/employees`);
+        return axios.get(`${EMPLOYEE_API_BASE_URL}`);
     }
 
     getEmployee = (id) => {
-        return axios.get(`${EMPLOYEE_API_BASE_URL}/employee/${id}`);
+        return axios.get(`${EMPLOYEE_API_BASE_URL}/${id}`);
     }
 
     updateEmployee = (id, employee) => {
-        return axios.put(`${EMPLOYEE_API_BASE_URL}/employee/${id}`, employee);
+        return axios.put(`${EMPLOYEE_API_BASE_URL}/${id}`, employee);
     }
 
     deleteEmployee = (id) => {
-        return axios.delete(`${EMPLOYEE_API_BASE_URL}/employees/${id}`);
+        return axios.delete(`${EMPLOYEE_API_BASE_URL}/${id}`);
     }
 }
 
